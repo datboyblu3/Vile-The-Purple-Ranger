eb6cfcd6a7bd5348204dc09696b0eea885855b6f
datboyblu3 <datboyblu3@protonmail.com>
2024-05-04 17:53:47 -0400

    Restructuring files

--- lab/terraform/modules/DigitalOcean/0-providers.tf
+++ lab/terraform/modules/DigitalOcean/0-providers.tf
@@ -10,3 +10,4 @@ terraform {
 provider "digitalocean" {
   token = var.do_token
 }
+
--- lab/terraform/modules/DigitalOcean/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ip_address" {
-  value = digitalocean_droplet.wazuh.ipv4_address
-  description = "The public IP address of your droplet application."
-}
