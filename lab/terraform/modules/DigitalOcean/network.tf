e6ac42a99caacdbad1d412d9779afdeaf7dadacf
Daniel Edwards <95729902+datboyblu3@users.noreply.github.com>
2024-05-04 10:48:52 -0400

    Update network.tf

--- lab/terraform/modules/DigitalOcean/network.tf
+++ lab/terraform/modules/DigitalOcean/network.tf
@@ -12,4 +12,82 @@ resource "digitalocean_vpc" "vpc"{
     ip_range = "192.168.32.0/24"
 }
 
+################################################################################
+# Firewall Rules for our Webserver Droplets                                    #
+################################################################################
+resource "digitalocean_firewall" "firewall" {
 
+    # The name we give our firewall for ease of use                            #    
+    name = "minimal-vpc-only-vpc-traffic"
+
+    # The droplets to apply this firewall to                                   #
+    droplet_ids = digitalocean_droplet.web.*.id
+
+    #--------------------------------------------------------------------------#
+    # Internal VPC Rules. We have to let ourselves talk to each other          #
+    #--------------------------------------------------------------------------#
+    inbound_rule {
+        protocol = "tcp"
+        port_range = "1-65535"
+        source_addresses = [digitalocean_vpc.vpc.ip_range]
+    }
+
+    inbound_rule {
+        protocol = "udp"
+        port_range = "1-65535"
+        source_addresses = [digitalocean_vpc.vpc.ip_range]
+    }
+
+    inbound_rule {
+        protocol = "icmp"
+        source_addresses = [digitalocean_vpc.vpc.ip_range]
+    }
+
+    outbound_rule {
+        protocol = "udp"
+        port_range = "1-65535"
+        destination_addresses = [digitalocean_vpc.vpc.ip_range]
+    }
+
+    outbound_rule {
+        protocol = "tcp"
+        port_range = "1-65535"
+        destination_addresses = [digitalocean_vpc.vpc.ip_range]
+    }
+
+    outbound_rule {
+        protocol = "icmp"
+        destination_addresses = [digitalocean_vpc.vpc.ip_range]
+    }
+
+    #--------------------------------------------------------------------------#
+    # Selective Outbound Traffic Rules                                         #
+    #--------------------------------------------------------------------------#
+
+    # DNS
+    outbound_rule {
+        protocol = "udp"
+        port_range = "53"
+        destination_addresses = ["0.0.0.0/0", "::/0"]
+    }
+
+    # HTTP
+    outbound_rule {
+        protocol = "tcp"
+        port_range = "80"
+        destination_addresses = ["0.0.0.0/0", "::/0"]
+    }
+
+    # HTTPS
+    outbound_rule {
+        protocol = "tcp"
+        port_range = "443"
+        destination_addresses = ["0.0.0.0/0", "::/0"]
+    }
+
+    # ICMP (Ping)
+    outbound_rule {
+        protocol              = "icmp"
+        destination_addresses = ["0.0.0.0/0", "::/0"]
+    }
+}
